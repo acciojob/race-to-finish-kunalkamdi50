9db99e8331ce891daed2ca9624a4725e1c0874ee
kunalkamdi50 <128037794+kunalkamdi50@users.noreply.github.com>
2023-05-01 14:51:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
-// window.promises = [];
+window.promises = [];
 // Define an array of five promises, each of which resolves with a random time between 1 and 5 seconds
 // Create an array of five promises that each resolve with a random time between 1 and 5 seconds
 // Define an array of five promises, each of which resolves with a random time between 1 and 5 seconds
-const promises = [
-  new Promise(resolve => setTimeout(() => resolve("Promise 1 resolved"), Math.floor(Math.random() * 5000 + 1000)),
-  new Promise(resolve => setTimeout(() => resolve("Promise 2 resolved"), Math.floor(Math.random() * 5000 + 1000)),
-  new Promise(resolve => setTimeout(() => resolve("Promise 3 resolved"), Math.floor(Math.random() * 5000 + 1000)),
-  new Promise(resolve => setTimeout(() => resolve("Promise 4 resolved"), Math.floor(Math.random() * 5000 + 1000)),
-  new Promise(resolve => setTimeout(() => resolve("Promise 5 resolved"), Math.floor(Math.random() * 5000 + 1000))
-];
+let x =Math.floor(Math.random()*5)+1;
+console.log(x);
+let promise1 = new Promise ((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("1");
+	},x * 1000)
+})
+let promise1 = new Promise ((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("2");
+	},x * 1000)
+})
+let promise1 = new Promise ((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("3");
+	},x * 1000)
+})
+let promise1 = new Promise ((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("4");
+	},x * 1000)
+})
+let promise1 = new Promise ((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("5");
+	},x * 1000)
+})
 
-// Use Promise.any to wait for the first promise to resolve, and then print its result to the div of id output.
-Promise.any(promises)
-  .then(result) => {
-     document.getElementById("output").innerText = result;
-  })
-  
+
+promises.push(promise1);
+promises.push(promise2);
+promises.push(promise3);
+promises.push(promise4);
+promises.push(promise5);
+
+Promise.any(Promises).then((value)=>{
+	document.getElementById("output").innerText = value;
+})
 
 
 
