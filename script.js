f4d9c785dfd76be9e24412c4d20061d15fe2a36f
kunalkamdi50 <128037794+kunalkamdi50@users.noreply.github.com>
2023-05-01 14:39:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 // Create an array of five promises that each resolve with a random time between 1 and 5 seconds
 // Define an array of five promises, each of which resolves with a random time between 1 and 5 seconds
 const promises = [
-  new Promise(resolve => setTimeout(() => resolve(1), Math.random() * 4000 + 1000)),
-  new Promise(resolve => setTimeout(() => resolve(2), Math.random() * 4000 + 1000)),
-  new Promise(resolve => setTimeout(() => resolve(3), Math.random() * 4000 + 1000)),
-  new Promise(resolve => setTimeout(() => resolve(4), Math.random() * 4000 + 1000)),
-  new Promise(resolve => setTimeout(() => resolve(5), Math.random() * 4000 + 1000))
+  new Promise(resolve => setTimeout(() => resolve("Promise 1 resolved"), Math.floor(Math.random() * 5000 + 1000)),
+  new Promise(resolve => setTimeout(() => resolve("Promise 2 resolved"), Math.floor(Math.random() * 5000 + 1000)),
+  new Promise(resolve => setTimeout(() => resolve("Promise 3 resolved"), Math.floor(Math.random() * 5000 + 1000)),
+  new Promise(resolve => setTimeout(() => resolve("Promise 4 resolved"), Math.floor(Math.random() * 5000 + 1000)),
+  new Promise(resolve => setTimeout(() => resolve("Promise 5 resolved"), Math.floor(Math.random() * 5000 + 1000))
 ];
 
 // Use Promise.any to wait for the first promise to resolve, and then print its result to the div of id output.
