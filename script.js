8ce3b42e9a9d066fd47dde94284372db7b98dd83
kunalkamdi50 <128037794+kunalkamdi50@users.noreply.github.com>
2023-05-01 10:38:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-window.promises = [];
+// window.promises = [];
+// Define an array of five promises, each of which resolves with a random time between 1 and 5 seconds
+const promises = [
+  new Promise(resolve => setTimeout(() => resolve(1), Math.random() * 4000 + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(2), Math.random() * 4000 + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(3), Math.random() * 4000 + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(4), Math.random() * 4000 + 1000)),
+  new Promise(resolve => setTimeout(() => resolve(5), Math.random() * 4000 + 1000))
+];
+
+// Use Promise.any to wait for the first promise to resolve, and then print its result to the div of id output.
+Promise.any(promises)
+  .then(result => {
+    const outputDiv = document.getElementById("output");
+    outputDiv.textContent = `The first resolved promise's result is ${result}`;
+  })
+  .catch(error => {
+    console.log(error); // This block will only execute if all of the promises reject.
+  });
+
 
 // Do not change the code above this
 // add your promises to the array `promises`
