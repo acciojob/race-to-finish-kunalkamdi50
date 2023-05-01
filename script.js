2bc3b51d674cb851b05cc5c74fbf3fdf0c488c42
kunalkamdi50 <128037794+kunalkamdi50@users.noreply.github.com>
2023-05-01 11:59:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 // window.promises = [];
 // Define an array of five promises, each of which resolves with a random time between 1 and 5 seconds
-const promises = [
-  new Promise(resolve => setTimeout(() => resolve(1), Math.random() * 4000 + 1000)),
-  new Promise(resolve => setTimeout(() => resolve(2), Math.random() * 4000 + 1000)),
-  new Promise(resolve => setTimeout(() => resolve(3), Math.random() * 4000 + 1000)),
-  new Promise(resolve => setTimeout(() => resolve(4), Math.random() * 4000 + 1000)),
-  new Promise(resolve => setTimeout(() => resolve(5), Math.random() * 4000 + 1000))
-];
+// Create an array of five promises that each resolve with a random time between 1 and 5 seconds
+const promises = Array.from({length: 5}, () => 
+  new Promise(resolve => setTimeout(() => {
+    const result = Math.floor(Math.random() * 5) + 1;
+    resolve(result);
+  }, Math.floor(Math.random() * 5000) + 1000))
+);
 
-// Use Promise.any to wait for the first promise to resolve, and then print its result to the div of id output.
+// Use Promise.any to wait for the first promise to resolve, and then print its result to the div of id output
 Promise.any(promises)
   .then(result => {
-    const outputDiv = document.getElementById("output");
-    outputDiv.textContent = `The first resolved promise's result is ${result}`;
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = `The first promise to resolve returned: ${result}`;
   })
   .catch(error => {
     console.log(error); // This block will only execute if all of the promises reject.
